ae00a462ff24d46b46df7cc3d0cee2597847abbe
Alonsiwisky <alonvillaram123@outlook.com>
2025-11-07 23:15:39 -0600

    imgs

--- mi-congreso/src/main.jsx
+++ mi-congreso/src/main.jsx
@@ -9,3 +9,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('Service Worker registrado'))
+      .catch(err => console.error('Error al registrar el SW:', err))
+  })
+}
